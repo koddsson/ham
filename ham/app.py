e660616f9e2b73f7242575ece0e4c5ea35b28017
Kristján Oddsson <koddsson@gmail.com>
2014-09-19 23:44:44 +0000

    Initial

--- ham/app.py
+++ ham/app.py
@@ -1,52 +1,68 @@
-import db
-import devops
-from flask import Flask, jsonify, abort, make_response, request
-from datetime import datetime, timedelta
+from datetime import timedelta
+from fabric.api import execute, env
+from functools import update_wrapper
+from flask import make_response, request, current_app, Flask, jsonify
 
 app = Flask(__name__)
 
+env.warn_only = True
 
-@app.route('/api/hosts', methods=['GET'])
-def get_hosts():
-    hosts = db.get_hosts()
-    if request.args.get('nocache', False):
-        hosts_to_update = []
-        now = datetime.now()
-        for host in hosts:
-            if now - host["cache_time"] > timedelta(minutes=1):
-                hosts_to_update.append(host["host_name"])
-        updated_hosts = devops.fetch_hosts_info(hosts_to_update)
-        db.update_hosts(updated_hosts)
-
-    return jsonify({"hosts": db.get_hosts()})
+package_name = 'azazofabric'
 
 
-@app.route('/api/hosts/<int:host_id>', methods=['GET'])
-def get_host(host_id):
-    hosts = db.get_hosts()
-    host = next((h for h in hosts if h['id'] == host_id), None)
-    if not host:
-        abort(404)
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
 
-    if (request.args.get('nocache', False) and
-       datetime.now() - host["cache_time"] > timedelta(minutes=1)):
-        updated_hosts = devops.fetch_hosts_info([host["host_name"]])
-        db.update_hosts(updated_hosts)
-        host = next((h for h in db.get_hosts() if h['id'] == host_id), None)
+    def get_methods():
+        if methods is not None:
+            return methods
 
-    return jsonify({'host': host})
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 
-@app.route('/')
-def index():
-    return "<img src=\"static/ham.jpg\"></img>"
-
+@app.route('/run/<host>/<task>', methods=['GET'])
+@crossdomain(origin='*')
+def run_command(host, task):
+    func = getattr(__import__(package_name), task)
+    results = execute(func, hosts=[host])
+    return jsonify({'ok': True, 'results': results})
 
 
 @app.errorhandler(404)
 def not_found(_):
     return make_response(jsonify({'error': 'Unable to find resource'}), 404)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- ham/update_hosts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /home/axel/work/devops
-. bin/activate
-fab -H azazo.coredata.is coredata.version
